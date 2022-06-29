520b973f442c50a4da15c4f65f57d527b4a0c23a
Ddrubok <yadomii@naver.com>
2022-06-29 18:50:15 +0900

    8/100

--- Server/GameServer/ServerPacketHandler.h
+++ Server/GameServer/ServerPacketHandler.h
@@ -5,10 +5,48 @@ enum
 	S_TEST = 1
 };
 
-struct BuffData
+template<typename T, typename C>
+class PacketIterator
 {
-	uint64 buffId;
-	float remainTime;
+public:
+	PacketIterator(C& container, uint16 index) : _container(container), _index(index) { }
+
+	bool operator !=(const PacketIterator& other) const { return _index != other._index; }
+
+	const T& operator*() const { return _container[_index]; }
+	T& operator*() { return _container[_index]; }
+	T* operator->() { return &_container[_index]; }
+	PacketIterator& operator++() { _index++; return *this; }
+	PacketIterator operator++(int32) { PacketIterator ret = *this; ++_index; return ret; }
+
+
+private:
+	C& _container;
+	uint16 _index;
+};
+
+
+template<typename T>
+class PacketList
+{
+public:
+	PacketList() : _data(nullptr), _count(0) {}
+	PacketList(T* data, uint16 count) : _data(data), _count(count) {}
+
+	T& operator[] (uint16 index)
+	{
+		ASSERT_CRASH(index < _count);
+		return _data[index];
+	}
+
+	uint16 Count() { return _count; }
+
+	PacketIterator<T, PacketList<T>> begin() { return PacketIterator<T, PacketList<T>>(*this, 0); }
+	PacketIterator<T, PacketList<T>> end() { return PacketIterator<T, PacketList<T>>(*this, _count); }
+
+private:
+	T* _data;
+	uint16 _count;
 };
 
 class ServerPacketHandler
@@ -19,3 +57,37 @@ public:
 	static SendBufferRef Make_S_TEST(uint64 id, uint32 hp, uint16 attack, vector<BuffData> buffs, wstring name);
 };
 
+#pragma pack(1)
+
+// [ PKT_S_TEST ][BuffsListItem BuffsListItem BuffsListItem]
+struct PKT_S_TEST
+{
+	struct BuffsListItem
+	{
+		uint64 buffId;
+		float remainTime;
+	};
+
+	uint16 packetSize; // 공용 헤더
+	uint16 packetId; // 공용 헤더
+	uint64 id; // 8
+	uint32 hp; // 4
+	uint16 attack; // 2
+	uint16 buffsOffset;
+	uint16 buffsCount;
+
+	using BuffsList = PacketList<PKT_S_TEST::BuffsListItem>;
+
+	BuffsList GetBuffersList()
+	{
+		BYTE* data = reinterpret_cast<BYTE*>(this);
+		data += buffsOffset;
+		return BuffsList(reinterpret_cast<PKT_S_TEST::BuffsListItem*>(data), buffsCount);
+	}
+};
+
+class PKT_S_TEST_WRITE
+{
+
+};
+#pragma pack()
